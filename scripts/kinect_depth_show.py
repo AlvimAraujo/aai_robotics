96ef5d5c1f86b89d2ba242c9340344833e00ca56
arthurhdn <arthurhdn@hotmail.com>
2019-10-13 13:19:40 -0300

    kinect

--- scripts/kinect_depth_show.py
+++ scripts/kinect_depth_show.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-####################################################
-# CODIGO QUE MOSTRA A IMAGEM RBG VISTA PELO KINECT #
-####################################################
+######################################################
+# CODIGO QUE MOSTRA A IMAGEM DEPTH VISTA PELO KINECT #
+######################################################
 from __future__ import print_function # apenas para imprimir os erros, caso hajam
 import roslib
 import sys
 import rospy
 import cv2
+import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -16,31 +17,30 @@ class image_converter:
   def __init__(self):
       self.bridge = CvBridge()
       # No em que vai publicar a imagem convertida
-      self.image_pub = rospy.Publisher("aai_image_show",Image, queue_size=1)
+      self.image_pub = rospy.Publisher("aai_depth_show",Image, queue_size=1)
       # No em que vai subscrever a imagem a ser lida
-      self.image_sub = rospy.Subscriber("/sensor/kinect_rgb",Image,self.callback_rgb)
+      self.image_sub = rospy.Subscriber("/sensor/kinect_depth",Image,self.callback)
 
   # Callback do topico do kinect
-  def callback_rgb(self,data):
+  def callback(self,data):
     #try except caso hajam erros
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
     except CvBridgeError as e:
-      print(e)
-    # # Desenhar circulo
-    # cv2.circle(cv_image, (50,50), 10, 255)
-    cv2.imshow("Kinect RGB", cv_image)
+        print(e)
+    depth_array = np.array(cv_image, dtype=np.float32)
+    cv2.normalize(depth_array, depth_array, 0, 1, cv2.NORM_MINMAX)
+    cv2.imshow("Kinect Depth", depth_array)
     cv2.waitKey(3)
 
-    #try except caso hajam erros
     try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough"))
     except CvBridgeError as e:
       print(e)
 
 def main(args):
   ic = image_converter()
-  rospy.init_node('kinect_rgb_show', anonymous=True)
+  rospy.init_node('kinect_depth_show', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:
