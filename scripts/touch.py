1fe30b1f1dc96b931d865cbfa9560eba6127ed32
ara1557 <alvimdlcd3@gmail.com>
2019-10-18 09:06:17 -0300

    touch working

--- scripts/touch.py
+++ scripts/touch.py
@@ -12,23 +12,28 @@ from tf.transformations import euler_from_quaternion
 class RosiNodeClass():
 
     def __init__(self):
+        # Variaveis que controlam a rotina de toque
         self.touch = 0
         self.state = 0
+
+        # Variavel que alerta sobre a forca
+        self.forceFlag = 0
+        
         # Comandos a serem enviados para as juntas
-        self.joint1 = 0
-        self.joint2 = 0
-        self.joint3 = 0
-        self.joint4 = 0
-        self.joint5 = 0
-        self.joint6 = 0
+        self.joint1 = 0.0
+        self.joint2 = 0.0
+        self.joint3 = 0.0
+        self.joint4 = 0.0
+        self.joint5 = 0.0
+        self.joint6 = 0.0
 
         # Posicao desejada das juntas
-        self.desired_joint1 = 0
-        self.desired_joint2 = 0
-        self.desired_joint3 = 0
-        self.desired_joint4 = 0
-        self.desired_joint5 = 0
-        self.desired_joint6 = 0
+        self.desired_joint1 = 0.0
+        self.desired_joint2 = 0.0
+        self.desired_joint3 = 0.0
+        self.desired_joint4 = 0.0
+        self.desired_joint5 = 0.0
+        self.desired_joint6 = 0.0
 
         # Publica em jointsPosTargetCommand
         self.pub_jointsPos = rospy.Publisher('/ur5/jointsPosTargetCommand',  ManipulatorJoints, queue_size=1)
@@ -55,6 +60,117 @@ class RosiNodeClass():
             node_sleep_rate.sleep()
 
 
+    # Funcao de callback do sensor de forca
+    def callback_force(self, data):
+        # Calculo e analise da forca
+        force = sqrt(data.twist.linear.x**2 + data.twist.linear.y**2 + data.twist.linear.z**2)
+        #force = data.twist.linear.x
+        if force >= 2:
+            print('forceFlag!!')
+            self.forceFlag = 1
+        else:
+            self.forceFlag = 0
+
+    # Funcao de callback das juntas
+    def callback_joints(self, data):
+        # Erro permitido
+        self.err = 0.05
+
+        # Posicao atual das juntas
+        self.actual1 = data.joint_variable[0]
+        self.actual2 = data.joint_variable[1]
+        self.actual3 = data.joint_variable[2]
+        self.actual4 = data.joint_variable[3]
+        self.actual5 = data.joint_variable[4]
+        self.actual6 = data.joint_variable[5]
+        
+        # Checa se a rotina de toque ja foi realizada uma vez
+        if(self.forceFlag == 1):
+            self.touch = 1
+
+        # No ponto especifico inicia a rotina
+        #if(abs(self.pos_x - 0) >= self.err and abs(self.pos_y - 0) >= self.err and abs(self.angle - 0) >= self.err and self.touch != 1):
+        
+        if(abs(self.actual1 - pi/2) <= self.err and abs(self.actual5 + pi/2) <= self.err and self.state == 0):
+            self.state = 1
+            print(self.state)
+        elif(abs(self.actual2 + pi/3) <= self.err and abs(self.actual4 - pi/3) <= self.err and self.state == 1):
+            self.state = 2
+            print(self.state)
+        elif(abs(self.actual3 - pi/3) <= self.err and abs(self.actual4 - 0) <= self.err and self.state == 2):
+            self.state = 3
+            print(self.state)
+        elif(abs(self.actual2 - 0) <= self.err and abs(self.actual4 + pi/3) <= self.err and self.state == 3):
+            self.state = 4
+            print(self.state)
+
+        if(self.state == 0):
+            print('estado 0')
+            self.desired_joint1 = pi/2
+            self.desired_joint2 = 0.0
+            self.desired_joint3 = 0.0
+            self.desired_joint4 = 0.0
+            self.desired_joint5 = -pi/2
+            self.desired_joint6 = 0.0
+        if(self.state == 1):
+            print('estado 1')
+            self.desired_joint1 = self.joint1
+            self.desired_joint2 = -pi/3
+            self.desired_joint3 = self.joint3
+            self.desired_joint4 = pi/3
+            self.desired_joint5 = self.joint5
+            self.desired_joint6 = self.joint6
+        elif(self.state == 2):
+            print('estado 2')
+            self.desired_joint1 = self.joint1
+            self.desired_joint2 = self.joint2
+            self.desired_joint3 = pi/3
+            self.desired_joint4 = 0.0
+            self.desired_joint5 = self.joint5
+            self.desired_joint6 = self.joint6
+        elif(self.state == 3):
+            print('estado 3')
+            self.desired_joint1 = self.joint1
+            self.desired_joint2 = 0.0
+            self.desired_joint3 = self.joint3
+            self.desired_joint4 = -pi/3
+            self.desired_joint5 = self.joint5
+            self.desired_joint6 = self.joint6
+        elif(self.state == 4):
+            print('estado 4')
+            self.desired_joint1 = self.joint1
+            self.desired_joint2 = pi/2
+            self.desired_joint3 = self.joint3
+            self.desired_joint4 = -5*pi/6
+            self.desired_joint5 = self.joint5
+            self.desired_joint6 = self.joint6
+        
+        # Volta as juntas para a posicao inicial
+        if(self.touch == 1):
+            print('estado 5')
+            self.state = 5
+            self.desired_joint2 = 0.0
+            self.desired_joint3 = 0.0
+            self.desired_joint4 = 0.0
+            if(abs(self.actual2 - 0) >= self.err and abs(self.actual3 - 0) >= self.err and abs(self.actual4 - 0) >= self.err):
+                self.desired_joint1 = 0.0
+                self.desired_joint5 = 0.0
+
+
+        #if(abs(self.desired_joint1 - self.actual1) >= self.err):
+        self.joint1 = self.desired_joint1
+        #if(abs(self.desired_joint2 - self.actual2) >= self.err):
+        self.joint2 = self.desired_joint2
+        #if(abs(self.desired_joint3 - self.actual3) >= self.err):
+        self.joint3 = self.desired_joint3
+        #if(abs(self.desired_joint4 - self.actual4) >= self.err):
+        self.joint4 = self.desired_joint4
+        #if(abs(self.desired_joint5 - self.actual5) >= self.err):
+        self.joint5 = self.desired_joint5
+        #if(abs(self.desired_joint6 - self.actual6) >= self.err):
+        self.joint6 = self.desired_joint6
+
+	
     # Callback da posicao
 	def callback_pose(self, data):
 
@@ -69,89 +185,12 @@ class RosiNodeClass():
 		self.pos_y = data.position.y
 		self.angle = euler_angles[2] # Apenas o angulo de Euler no eixo z interessa
 
-    # Funcao de callback das juntas
-    def callback_joints(self, data):
-        # Erro permitido
-        self.err = 0.05
-        
-        
-        # Checa se a rotina de toque ja foi realizada uma vez
-        if(self.forceFlag == 1):
-            self.touch = 1
-
-        # No ponto especifico inicia a rotina
-        #if(abs(self.pos_x - 0) >= self.err and abs(self.pos_y - 0) >= self.err and abs(self.angle - 0) >= self.err and self.touch != 1):
-        self.desired_joint1 = pi/2
-        self.desired_joint5 = -pi/2
-        if(abs(data.joint_variable[0] - pi/2) >= self.err and abs(data.joint_variable[4] + pi/2) >= self.err):
-            self.state = 1
-            print('1')
-        if(self.state == 1):
-            self.desired_joint2 = -pi/3
-            self.desired_joint4 = pi/3
-        if(abs(data.joint_variable[1] + pi/3) >= self.err and abs(data.joint_variable[3] - pi/3) >= self.err):
-            self.state = 2
-            print('2')
-        if(self.state == 2):
-            self.desired_joint3 = pi/3
-            self.desired_joint4 = 0
-        if(abs(data.joint_variable[2] - pi/3) >= self.err and abs(data.joint_variable[3] - 0) >= self.err):
-            self.state = 3
-            print('3')
-        if(self.state == 3):
-            self.desired_joint2 = 0
-            self.desired_joint4 = -pi/3
-        if(abs(data.joint_variable[1] - 0) >= self.err and abs(data.joint_variable[3] + pi/3) >= self.err):
-            self.state = 4
-            print('4')
-        if(self.state == 4):
-            self.desired_joint2 = pi/2
-            self.desired_joint4 = -5*pi/6
-        
-        # Volta as juntas para a posicao inicial
-        if(self.touch == 1):
-            self.desired_joint2 = 0
-            self.desired_joint3 = 0
-            self.desired_joint4 = 0
-            if(abs(data.joint_variable[1] - 0) >= self.err and abs(data.joint_variable[2] - 0) >= self.err and abs(data.joint_variable[3] - 0) >= self.err):
-                self.desired_joint1 = 0
-                self.desired_joint5 = 0
-                self.state = 0
-
-
-        if(abs(self.desired_joint1 - data.joint_variable[0]) >= self.err):
-            self.joint1 = self.desired_joint1
-        if(abs(self.desired_joint2 - data.joint_variable[1]) >= self.err):
-            self.joint2 = self.desired_joint2
-        if(abs(self.desired_joint3 - data.joint_variable[2]) >= self.err):
-            self.joint3 = self.desired_joint3
-        if(abs(self.desired_joint4 - data.joint_variable[3]) >= self.err):
-            self.joint4 = self.desired_joint4
-        if(abs(self.desired_joint5 - data.joint_variable[4]) >= self.err):
-            self.joint5 = self.desired_joint5
-        if(abs(self.desired_joint6 - data.joint_variable[5]) >= self.err):
-            self.joint6 = self.desired_joint6
-        
-    # Funcao de callback do sensor de forca
-    def callback_force(self, data):
-        # Variavel que alerta sobre a forca
-        self.forceFlag = 0
-        # Calculo e analise da forca
-        force = sqrt(data.twist.linear.x**2 + data.twist.linear.y**2 + data.twist.linear.z**2)
-        if force >= 0.9:
-            self.forceFlag = 1
-        else:
-            self.forceFlag = 0
-
-
-	
-
 
 
 if __name__ == '__main__':
 
     # Inicializa o no
-    rospy.init_node('rosi_ur5_control', anonymous=True)
+    rospy.init_node('rosi_touch_control', anonymous=True)
 
     # Inicializa o objeto
     try:
