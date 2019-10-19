fa6669fd1c951f054af90b28d0ea6ec2ff864c6f
ara1557 <alvimdlcd3@gmail.com>
2019-10-19 14:14:03 -0300

    toque com estado 5 melhorado

--- scripts/touch.py
+++ scripts/touch.py
@@ -38,10 +38,11 @@ class RosiNodeClass():
         # Publica em jointsPosTargetCommand
         self.pub_jointsPos = rospy.Publisher('/ur5/jointsPosTargetCommand',  ManipulatorJoints, queue_size=1)
 
+        self.subpinto = rospy.Subscriber('/aai_rosi_pose', Pose, self.callback_pose)
+
         # Subscreve em jointsPositionCurrentState, forceTorqueSensorOutput e aai_rosi_pose
         self.sub_joints = rospy.Subscriber('/ur5/jointsPositionCurrentState', ManipulatorJoints, self.callback_joints)
         self.sub_force = rospy.Subscriber('/ur5/forceTorqueSensorOutput', TwistStamped, self.callback_force)
-        #self.sub_pose = rospy.Subscriber('/aai_rosi_pose', Pose, self.callback_pose)
 
         # Define a frequencia de publicacao
         node_sleep_rate = rospy.Rate(10)
@@ -61,11 +62,22 @@ class RosiNodeClass():
 
 
     # Funcao de callback do sensor de forca
+    def callback_pose(self, data):
+		q_x = data.orientation.x
+		q_y = data.orientation.y
+		q_z = data.orientation.z
+		q_w = data.orientation.w
+		# Orientacao de quaternios para angulos de Euler
+		euler_angles = euler_from_quaternion([q_x, q_y, q_z, q_w])
+
+		self.pos_x  = data.position.x
+		self.pos_y = data.position.y
+		self.angle = euler_angles[2] # Apenas o angulo de Euler no eixo z interessa
+
     def callback_force(self, data):
         # Calculo e analise da forca
         force = sqrt(data.twist.linear.x**2 + data.twist.linear.y**2 + data.twist.linear.z**2)
-        #force = data.twist.linear.x
-        if force >= 2:
+        if force >= 1.5:
             print('forceFlag!!')
             self.forceFlag = 1
         else:
@@ -84,7 +96,7 @@ class RosiNodeClass():
         self.actual5 = data.joint_variable[4]
         self.actual6 = data.joint_variable[5]
         
-        # Checa se a rotina de toque ja foi realizada uma vez
+        # Checa se o toque já ocorreu
         if(self.forceFlag == 1):
             self.touch = 1
 
@@ -120,7 +132,7 @@ class RosiNodeClass():
             self.desired_joint4 = pi/3
             self.desired_joint5 = self.joint5
             self.desired_joint6 = self.joint6
-        elif(self.state == 2):
+        if(self.state == 2):
             print('estado 2')
             self.desired_joint1 = self.joint1
             self.desired_joint2 = self.joint2
@@ -128,7 +140,7 @@ class RosiNodeClass():
             self.desired_joint4 = 0.0
             self.desired_joint5 = self.joint5
             self.desired_joint6 = self.joint6
-        elif(self.state == 3):
+        if(self.state == 3):
             print('estado 3')
             self.desired_joint1 = self.joint1
             self.desired_joint2 = 0.0
@@ -136,7 +148,7 @@ class RosiNodeClass():
             self.desired_joint4 = -pi/3
             self.desired_joint5 = self.joint5
             self.desired_joint6 = self.joint6
-        elif(self.state == 4):
+        if(self.state == 4):
             print('estado 4')
             self.desired_joint1 = self.joint1
             self.desired_joint2 = pi/2
@@ -150,41 +162,20 @@ class RosiNodeClass():
             print('estado 5')
             self.state = 5
             self.desired_joint2 = 0.0
-            self.desired_joint3 = 0.0
-            self.desired_joint4 = 0.0
-            if(abs(self.actual2 - 0) >= self.err and abs(self.actual3 - 0) >= self.err and abs(self.actual4 - 0) >= self.err):
+            if(abs(self.actual2 - 0) >= self.err):
+                self.desired_joint3 = 0.0
+                self.desired_joint4 = 0.0
+            if(abs(self.actual3 - 0) >= self.err and abs(self.actual4 - 0) >= self.err):
                 self.desired_joint1 = 0.0
                 self.desired_joint5 = 0.0
 
 
-        #if(abs(self.desired_joint1 - self.actual1) >= self.err):
         self.joint1 = self.desired_joint1
-        #if(abs(self.desired_joint2 - self.actual2) >= self.err):
         self.joint2 = self.desired_joint2
-        #if(abs(self.desired_joint3 - self.actual3) >= self.err):
         self.joint3 = self.desired_joint3
-        #if(abs(self.desired_joint4 - self.actual4) >= self.err):
         self.joint4 = self.desired_joint4
-        #if(abs(self.desired_joint5 - self.actual5) >= self.err):
         self.joint5 = self.desired_joint5
-        #if(abs(self.desired_joint6 - self.actual6) >= self.err):
         self.joint6 = self.desired_joint6
-
-	
-    # Callback da posicao
-	def callback_pose(self, data):
-
-		q_x = data.orientation.x
-		q_y = data.orientation.y
-		q_z = data.orientation.z
-		q_w = data.orientation.w
-		# Orientacao de quaternios para angulos de Euler
-		euler_angles = euler_from_quaternion([q_x, q_y, q_z, q_w])
-
-		self.pos_x  = data.position.x
-		self.pos_y = data.position.y
-		self.angle = euler_angles[2] # Apenas o angulo de Euler no eixo z interessa
-
 
 
 if __name__ == '__main__':
